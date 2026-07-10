63ccb21ecdcf388226cb8971cb3db6c2f66ae95e
deepak445566 <deepakpandey1212d@gmail.com>
2026-07-10 22:31:30 +0530

    ok

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
-// eslint.config.mjs
-export default {
-  rules: {
-    'react/no-unescaped-entities': 'off',
-  }
-}
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  {
+    rules: {
+      // Disable the rule for unescaped entities
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+
+export default eslintConfig
